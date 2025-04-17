e4c8522289725e34e56be1d9276e8a083d4e9193
Shema <shemaphilbert8@gmail.com>
2025-04-17 15:25:49 +0200

    async learning

---
+++ promise1.js
@@ -0,0 +1,20 @@
+// promise1.js
+
+let pizzaOrder = new Promise(function(resolve, reject) {
+    let isPizzaReady = false;
+  
+    if (isPizzaReady) {
+      resolve("Your pizza is here! 🍕");
+    } else {
+      reject("No pizza for you 😢");
+    }
+  });
+  
+  pizzaOrder
+    .then(function(message) {
+      console.log("Success:", message);
+    })
+    .catch(function(error) {
+      console.log("Error:", error);
+    });
+  
